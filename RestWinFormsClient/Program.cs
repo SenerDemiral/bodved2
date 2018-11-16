f7aeac116db16ab9468ea4aefd7867ff27dd4bc5
SenerDemiral <sener.demiral@gmail.com>
2018-11-16 23:12:34 +0300

    181116

--- RestWinFormsClient/Program.cs
+++ RestWinFormsClient/Program.cs
@@ -29,8 +29,8 @@ namespace RestWinFormsClient
 
     public static class grpcService
     {
-        public static Channel channel = new Channel($"127.0.0.1:6000", ChannelCredentials.Insecure);
-        //public static Channel channel = new Channel($"217.160.13.102:6000", ChannelCredentials.Insecure);
+        //public static Channel channel = new Channel($"127.0.0.1:6000", ChannelCredentials.Insecure);
+        public static Channel channel = new Channel($"217.160.13.102:6000", ChannelCredentials.Insecure);
         public static CRUDs.CRUDsClient ClientCRUDs = new CRUDs.CRUDsClient(channel);
     }
 
